0d8cf0504d11618673837ce693ba527e646fb168
edherbert <edward@othermythos.com>
2022-12-23 15:26:34 +0000

    Improvements to the texture coords generation procedure.

--- util/voxelConverter.py
+++ util/voxelConverter.py
@@ -33,8 +33,8 @@ class XMLWriter:
 
             t = self.data.texCoords[self.data.vertColours[i]]
             texcoord = ET.SubElement(vert, "texcoord")
-            texcoord.attrib["u"] = str(t)
-            texcoord.attrib["v"] = "0.5"
+            texcoord.attrib["u"] = str(t[0])
+            texcoord.attrib["v"] = str(t[1])
 
     def writeToFile(self, filePath):
         materialName = "baseVoxelMaterial"
@@ -84,6 +84,62 @@ class CompleteData:
         self.faces = []
         self.texCoords = []
 
+        self.colData = [
+            [0.000000, 0.000000, 0.000000],
+            [0.133333, 0.125490, 0.203922],
+            [0.270588, 0.156863, 0.235294],
+            [0.400000, 0.223529, 0.192157],
+            [0.560784, 0.337255, 0.231373],
+            [0.874510, 0.443137, 0.149020],
+            [0.850980, 0.627451, 0.400000],
+            [0.933333, 0.764706, 0.603922],
+            [0.984314, 0.949020, 0.211765],
+            [0.600000, 0.898039, 0.313725],
+            [0.415686, 0.745098, 0.188235],
+            [0.215686, 0.580392, 0.431373],
+            [0.294118, 0.411765, 0.184314],
+            [0.321569, 0.294118, 0.141176],
+            [0.196078, 0.235294, 0.223529],
+            [0.247059, 0.247059, 0.454902],
+            [0.188235, 0.376471, 0.509804],
+            [0.356863, 0.431373, 0.882353],
+            [0.388235, 0.607843, 1.000000],
+            [0.372549, 0.803922, 0.894118],
+            [0.796078, 0.858824, 0.988235],
+            [1.000000, 1.000000, 1.000000],
+            [0.607843, 0.678431, 0.717647],
+            [0.517647, 0.494118, 0.529412],
+            [0.411765, 0.415686, 0.415686],
+            [0.349020, 0.337255, 0.321569],
+            [0.462745, 0.258824, 0.541176],
+            [0.674510, 0.196078, 0.196078],
+            [0.850980, 0.341176, 0.388235],
+            [0.843137, 0.482353, 0.729412],
+            [0.560784, 0.592157, 0.290196],
+            [0.541176, 0.435294, 0.188235],
+        ]
+        #Resolve the colours to their summed hash value for fast lookup.
+        #They're written like this above just to keep track of the original values.
+        for i in range(len(self.colData)):
+            self.colData[i] = sum(self.colData[i])
+
+        self.resolveTexCoords()
+
+    '''
+    Determine the coordinates relative to the texture.
+    '''
+    def resolveTexCoords(self):
+        numTilesWidth = 8
+        numTilesHeight = 4
+        tileWidth = (1.0 / numTilesWidth) / 2.0
+        tileHeight = (1.0 / numTilesHeight) / 2.0
+
+        self.texCoords = [None] * len(self.colData)
+        for i in range(len(self.colData)):
+            tileX = i % numTilesWidth
+            tileY = int(i / numTilesWidth)
+            self.texCoords[i] = (tileX / numTilesWidth + tileWidth, tileY / numTilesHeight + tileHeight)
+
     def addVert(self, vert):
         if len(vert) != 7:
             return False
@@ -117,19 +173,14 @@ class CompleteData:
         return True
 
     def resolveColours(self):
-        count = 0
         entries = {}
         for i in range(len(self.vertColours)):
             val = sum(self.vertColours[i])
             if not val in entries:
-                entries[val] = count
-                count += 1
+                targetIdx = self.colData.index(val)
+                entries[val] = targetIdx
             self.vertColours[i] = entries[val]
 
-        max = len(entries)
-        self.texCoords = [None] * max
-        for i in range(max):
-            self.texCoords[i] = (1.0 / max) * i
 
     '''
     Iterate all faces and match up the vertices with the normals.
