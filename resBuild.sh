976450c1a34e32d3dd5f389f9146cb843f49771c
edherbert <edward@othermythos.com>
2024-01-30 00:30:35 +0000

    Updates for actions.

--- resBuild.sh
+++ resBuild.sh
@@ -12,7 +12,7 @@ mkdir -p ${SCRIPT_DIR}/build/assets
 
 BUILD_OUTPUT=${SCRIPT_DIR}/build/assets
 BUILD_INPUT=${SCRIPT_DIR}/assets/
-docker run -it --name asset-builder-container --rm \
+docker run --name asset-builder-container --rm \
     -v "$BUILD_OUTPUT:$BUILD_OUTPUT" \
     -v "$BUILD_INPUT:$BUILD_INPUT" \
     asset-builder-image-voxel -m AssetModuleGox --input $BUILD_INPUT --output $BUILD_OUTPUT $*
