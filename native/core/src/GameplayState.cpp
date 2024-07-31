109957d07898741edaeab8abc74f4370c3b64c64
edherbert <edward@othermythos.com>
2024-07-31 18:07:23 +0100

    Properly reset found regions.

--- native/core/src/GameplayState.cpp
+++ native/core/src/GameplayState.cpp
@@ -8,6 +8,7 @@ namespace ProceduralExplorationGameCore{
 
     void GameplayState::setNewMapData(ExplorationMapData* mapData){
         mFoundRegions.resize(mapData->regionData.size(), false);
+        std::fill(mFoundRegions.begin(), mFoundRegions.end(), false);
     }
 
 }
