5715b75f044c8d2190f92052732e28681653fb9d
edherbert <edward@othermythos.com>
2024-11-23 18:26:56 +0000

    Fixes for build

--- native/core/src/GameplayConstants.cpp
+++ native/core/src/GameplayConstants.cpp
@@ -12,13 +12,15 @@ namespace ProceduralExplorationGameCore{
 
     void GameplayConstants::initialise(){
         mPlaces.resize((size_t)PlaceId::MAX);
-        #include "../../../src/Content/PlaceDefs.h.nut"
+        //#include "../../../src/Content/PlaceDefs.h.nut"
 
+        /*
         mPlacesByType.resize((size_t)PlaceType::MAX);
         for(size_t i = 0; i < (size_t)PlaceId::MAX; i++){
             const PlaceDef& p = mPlaces[i];
             mPlacesByType[(size_t)p.t].push_back(i);
         }
+         */
     }
 
 }
