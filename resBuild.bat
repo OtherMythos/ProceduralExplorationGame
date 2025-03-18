0da8c2eee421c1a35d9e88a9dbde3ea7849178a1
edherbert <edward@othermythos.com>
2025-03-18 19:19:41 +0000

    Updated resBuild.bat

--- resBuild.bat
+++ resBuild.bat
@@ -1,6 +1,6 @@
 SET SCRIPT_DIR=%~dp0
 
-docker build -t asset-builder-image-voxel util/
+docker pull ghcr.io/othermythos/asset-builder-image-voxel:latest
 
 ::Make sure the output exists.
 echo "Clearing build directory"
@@ -12,6 +12,6 @@ SET BUILD_INPUT="%SCRIPT_DIR%\assets"
 docker run -it --name asset-builder-container --rm ^
     -v "%BUILD_OUTPUT%:/buildOutput" ^
     -v "%BUILD_INPUT%:/buildInput" ^
-    asset-builder-image-voxel -m AssetModuleGox --input /buildInput --output /buildOutput %*
+    ghcr.io/othermythos/asset-builder-image-voxel:latest -m AssetModuleGox --input /buildInput --output /buildOutput %*
 
 @REM chmod -R 777 $BUILD_OUTPUT
