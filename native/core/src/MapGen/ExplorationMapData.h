50947356f15ec34b4f03aeb345cb21702a2669f6
edherbert <edward@othermythos.com>
2025-06-13 14:37:49 +0100

    Switched strings to references Removed unecessary code

--- native/core/src/MapGen/ExplorationMapData.h
+++ native/core/src/MapGen/ExplorationMapData.h
@@ -42,68 +42,39 @@ namespace ProceduralExplorationGameCore{
         std::map<std::string, MapDataEntry> mEntries;
     public:
 
-        //TODO switch all the strings to references.
-        MapDataReadResult readEntry(std::string name, MapDataEntry* outEntry, MapDataEntryType expectedType = MapDataEntryType::ANY) const;
+        MapDataReadResult readEntry(const std::string& name, MapDataEntry* outEntry, MapDataEntryType expectedType = MapDataEntryType::ANY) const;
 
-        void setEntry(std::string name, MapDataEntry entry);
+        void setEntry(const std::string& name, MapDataEntry entry);
 
-        AV::uint32 uint32(std::string name) const;
-        void uint32(std::string name, AV::uint32 val);
+        AV::uint32 uint32(const std::string& name) const;
+        void uint32(const std::string& name, AV::uint32 val);
 
-        size_t sizeType(std::string name) const;
-        void sizeType(std::string name, size_t val);
+        size_t sizeType(const std::string& name) const;
+        void sizeType(const std::string& name, size_t val);
 
-        void* voidPtr(std::string name) const;
-        void voidPtr(std::string name, void* val);
+        void* voidPtr(const std::string& name) const;
+        void voidPtr(const std::string& name, void* val);
 
         template <typename T>
-        T* ptr(std::string name){
+        T* ptr(const std::string& name){
             MapDataEntry out;
             MAP_ASSERT_RESULT(readEntry(name, &out, MapDataEntryType::VOID_PTR));
 
             return reinterpret_cast<T*>(out.value.ptr);
         }
 
-        WorldPoint worldPoint(std::string name) const;
-        void worldPoint(std::string name, WorldPoint val);
+        WorldPoint worldPoint(const std::string& name) const;
+        void worldPoint(const std::string& name, WorldPoint val);
     };
 
     class ExplorationMapData : public ExplorationMapDataBase{
-
     public:
-
-
-
-
         AV::uint32 width;
         AV::uint32 height;
-
-        //AV::uint32 seed;
-        //AV::uint32 moistureSeed;
-        //AV::uint32 variationSeed;
-
         AV::uint32 seaLevel;
-
-        //WorldPoint playerStart;
-        //WorldPoint gatewayPosition;
 
         void* voxelBuffer;
         void* secondaryVoxelBuffer;
         void* blueNoiseBuffer;
-        //size_t voxelBufferSize;
-        //size_t secondaryVoxelBufferSize;
-        //size_t blueNoiseBufferSize;
-        //void* riverBuffer;
-
-        //float* waterTextureBuffer;
-        //float* waterTextureBufferMask;
-
-        //std::vector<RegionData> regionData;
-        //std::vector<PlacedItemData> placedItems;
-        //TODO switch these to not be pointers.
-        //std::vector<FloodFillEntry*> waterData;
-        //std::vector<FloodFillEntry*> landData;
-        //std::vector<PlaceData> placeData;
-        //std::vector<RiverData> riverData;
     };
 }
