6edfca36b114fd33fd71b32dbe95991d0f453f30
edherbert <edward@othermythos.com>
2025-08-24 15:32:52 +0100

    Fix for build.

--- native/core/src/MapGen/BaseClient/Steps/PerformFinalFloodFillMapGenStep.cpp
+++ native/core/src/MapGen/BaseClient/Steps/PerformFinalFloodFillMapGenStep.cpp
@@ -74,7 +74,5 @@ namespace ProceduralExplorationGameCore{
                 }
             }
         }
-
-        return true;
     }
 }
--- native/core/src/MapGen/BaseClient/Steps/PerformPreFloodFillMapGenStep.cpp
+++ native/core/src/MapGen/BaseClient/Steps/PerformPreFloodFillMapGenStep.cpp
@@ -68,7 +68,5 @@ namespace ProceduralExplorationGameCore{
                 }
             }
         }
-
-        return true;
     }
 }
