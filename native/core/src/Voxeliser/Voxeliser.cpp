cf66f5b7e3f5b99a38f333715aa410c697666fc3
edherbert <edward@othermythos.com>
2026-02-06 15:56:19 +0000

    Fix for badly wrapped voxel mask.

--- native/core/src/Voxeliser/Voxeliser.cpp
+++ native/core/src/Voxeliser/Voxeliser.cpp
@@ -520,7 +520,7 @@ namespace ProceduralExplorationGameCore{
                     faceContainer.faceMask = f;
                     faceContainer.regionId = regionId;
                     faceContainer.flags = flags;
-                    AV::uint8 voxelDiffuse = *VOXEL_META_PTR_FOR_COORD(mapData, WRAP_WORLD_POINT(x, y)) & 0x3;
+                    AV::uint8 voxelDiffuse = *VOXEL_META_PTR_FOR_COORD(mapData, WRAP_WORLD_POINT(x, y)) & 0x7;
                     faceContainer.voxelDiffuse = voxelDiffuse;
 
                     outFaces.outFaces.push_back(faceContainer);
