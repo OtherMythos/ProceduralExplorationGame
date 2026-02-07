0e24a4f34b0e9f908dd4464248077f92947bd7df
edherbert <edward@othermythos.com>
2026-02-07 13:51:52 +0000

    Made merge small regions repeat until all small regions are merged

--- native/core/src/MapGen/BaseClient/Steps/MergeSmallRegionsMapGenStep.cpp
+++ native/core/src/MapGen/BaseClient/Steps/MergeSmallRegionsMapGenStep.cpp
@@ -20,27 +20,34 @@ namespace ProceduralExplorationGameCore{
     bool MergeSmallRegionsMapGenStep::processStep(const ExplorationMapInputData* input, ExplorationMapData* mapData, ExplorationMapGenWorkspace* workspace){
         std::vector<RegionData>& regionData = (*mapData->ptr<std::vector<RegionData>>("regionData"));
 
-        for(RegionData& d : regionData){
-            if(d.total >= 200) continue;
+        bool mergesOccurred = true;
+        while(mergesOccurred){
+            mergesOccurred = false;
 
-            //Don't attempt to merge small main regions, so the session can guarantee having the correct number.
-            if(d.meta & static_cast<AV::uint8>(RegionMeta::MAIN_REGION)){
-                continue;
-            }
+            for(RegionData& d : regionData){
+                if(d.total == 0) continue;
+                if(d.total >= 200) continue;
 
-            //Go through the edges and check the neighbours for a region
-            std::set<RegionId> foundRegions;
-            findNeighboursForRegion(mapData, d, foundRegions);
+                //Don't attempt to merge small main regions, so the session can guarantee having the correct number.
+                if(d.meta & static_cast<AV::uint8>(RegionMeta::MAIN_REGION)){
+                    continue;
+                }
 
-            for(RegionId r : foundRegions){
-                //TODO do I need this?
-                if(r == 0x0 || r == REGION_ID_WATER) continue;
-                RegionData& sd = regionData[r];
-                if(d.id == sd.id) continue;
-                mergeRegionData(mapData, d, sd);
+                //Go through the edges and check the neighbours for a region
+                std::set<RegionId> foundRegions;
+                findNeighboursForRegion(mapData, d, foundRegions);
 
-                //Just take the first value for now.
-                break;
+                for(RegionId r : foundRegions){
+                    //TODO do I need this?
+                    if(r == 0x0 || r == REGION_ID_WATER) continue;
+                    RegionData& sd = regionData[r];
+                    if(d.id == sd.id) continue;
+                    mergeRegionData(mapData, d, sd);
+                    mergesOccurred = true;
+
+                    //Just take the first value for now.
+                    break;
+                }
             }
         }
 
