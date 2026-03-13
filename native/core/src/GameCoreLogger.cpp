b75502b57f204f1c62db0ef0029e70d9140cf8c0
edherbert <edward@othermythos.com>
2026-03-13 15:30:49 +0000

    Ensured game core logger doesn't clash with the engine logger.

--- native/core/src/GameCoreLogger.cpp
+++ native/core/src/GameCoreLogger.cpp
@@ -3,12 +3,12 @@
 #ifdef WIN32
     #ifdef WIN_DESKTOP_APPLICATION
         #include "spdlog/sinks/msvc_sink.h"
-    #else
-        #include "spdlog/sinks/stdout_color_sinks.h"
     #endif
 #elif defined(TARGET_ANDROID)
     #include "spdlog/sinks/android_sink.h"
-#else
+#endif
+
+#if !defined(WIN32) && !defined(TARGET_ANDROID) || (defined(WIN32) && !defined(WIN_DESKTOP_APPLICATION))
     #include "spdlog/sinks/stdout_color_sinks.h"
 #endif
 
@@ -21,14 +21,20 @@ namespace ProceduralExplorationGameCore{
             #ifdef WIN_DESKTOP_APPLICATION
                 auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
                 _logger = std::make_shared<spdlog::logger>(loggerName, sink);
-            #else
-                _logger = spdlog::stdout_color_mt(loggerName);
             #endif
-
         #elif defined(TARGET_ANDROID)
             _logger = spdlog::android_logger_mt(loggerName);
-        #else
-            _logger = spdlog::stdout_color_mt(loggerName);
+        #endif
+
+        #if !defined(WIN32) && !defined(TARGET_ANDROID) || (defined(WIN32) && !defined(WIN_DESKTOP_APPLICATION))
+            //Share the engine's stdout sink so both loggers contend on the same mutex,
+            //preventing interleaved ANSI colour codes from concurrent threads.
+            auto engineLogger = spdlog::get("AV");
+            if(engineLogger && !engineLogger->sinks().empty()){
+                _logger = std::make_shared<spdlog::logger>(loggerName, engineLogger->sinks().begin(), engineLogger->sinks().end());
+            } else {
+                _logger = spdlog::stdout_color_mt(loggerName);
+            }
         #endif
 
         _logger->set_level(spdlog::level::trace);
